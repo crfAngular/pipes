9fc56e836a0538bb21bb43fce53082e1696ef6e8
Moisés Cid <moises.cid@ideit.es>
2020-08-31 13:05:02 +0200

    Inicio aplicación de PIPES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  fecha: Date = new Date();
+  nombre: string = 'Juan Pérez';
+  textoLargo: string = 'Texto largo del que solo quiero mostrar 10 caracteres';
+
+  precio: number = 4.5;
+
+  PI: number = Math.PI;
+
+  descuento: number = 0.25;
+
+  myObject = { foo: 'bar', baz: 'qux', nested: { xyz: 3, numbers: [1, 2, 3, 4, 5] } };
+
+  miPromesa = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Datos obtenidos de la API');
+    }, 2000);
+  });
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+registerLocaleData(localEs);
+registerLocaleData(localFr);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
